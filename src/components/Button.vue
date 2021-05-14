f5c32494b90c71227d59cbe081f5777ed84d98fd
eyasudev <mitchelljack979@gmail.com>
2021-05-14 08:22:18 +0500

    + Add Vue Crash Course Project DONE

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn" >{{text}}</button>    
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:{
+        text:{
+            type:String,
+            default:'Sample Buttn'
+        },
+        color:{
+            type:String,
+            default:'Sample Buttn'
+        }
+    },
+    methods: {
+        onClick() {
+            this.$emit('btn-click');
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button v-show="homePage" :text="showAddTask ? 'Close':'Add Task'"  @btn-click="$emit('toggle-add-task')" :color="showAddTask ? 'red':'green'" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  //props: ['title']
+  // props: {
+  //     title: String,
+  // }
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+    showAddTask: {
+      type:Boolean,
+      default: false,
+    }
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if ( this.$route.path == '/') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+Button;
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
